829eafd43e027f55e20439107378cfd1febebfbc
Ashutosh <Ashutosh>
2024-06-25 21:00:25 +0530

    Initialized spring project

---
+++ fundamentals/src/main/java/com/learning/fundamentals/SpringFundamentalsApplication.java
@@ -0,0 +1,14 @@
+package com.learning.fundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFundamentalsApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
---
+++ fundamentals/src/test/java/com/learning/fundamentals/SpringFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.fundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
