d29d717b4cdf9d52cfa88aecad9bb82eacce7867
Ashutosh <Ashutosh>
2024-09-02 09:38:48 +0530

    working with netflix hysterix and spring boot to manage client resiliency

--- spmia-licensing-service/organization-service/src/main/java/com/thoughtmechanix/organization/repository/OrganizationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.thoughtmechanix.organization.repository;
-
-import com.thoughtmechanix.organization.model.Organization;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrganizationRepository extends CrudRepository<Organization, String> {
-
-    Organization findById(String id);
-}
