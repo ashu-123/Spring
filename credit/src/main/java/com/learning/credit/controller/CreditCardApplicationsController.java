209caa32e724507525d1c6f5e82b2ae7b63eb41a
Ashutosh <Ashutosh>
2024-09-15 15:29:27 +0530

    Adding consumer end stub testing

--- credit/src/main/java/com/learning/credit/controller/CreditCardApplicationsController.java
+++ credit/src/main/java/com/learning/credit/controller/CreditCardApplicationsController.java
@@ -4,6 +4,7 @@ import com.learning.credit.model.ApplyForCreditCardRequest;
 import com.learning.credit.model.ApplyForCreditCardResponse;
 import com.learning.credit.model.CreditCheckRequest;
 import com.learning.credit.model.CreditCheckResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import static com.learning.credit.model.ApplyForCreditCardRequest.CardType.GOLD;
 import static com.learning.credit.model.ApplyForCreditCardResponse.Status.GRANTED;
@@ -26,21 +28,16 @@ public class CreditCardApplicationsController {
     }
 
     @PostMapping("/credit-card-applications")
-    public ApplyForCreditCardResponse applyForCreditCard(@RequestBody final ApplyForCreditCardRequest applyForCreditCardRequest) {
+    public ApplyForCreditCardResponse applyForCreditCard(@RequestBody final ApplyForCreditCardRequest applyForCreditCardRequest,
+                                                         @Value("${creditcheckservice.baseurl}") String baseUrl) {
         int citizenNumber = applyForCreditCardRequest.getCitizenNumber();
-        var creditCheckResponse = restTemplate.postForObject("http://localhost:8080/credit-scores",
+        String uri = UriComponentsBuilder.fromHttpUrl(baseUrl).path("credit-scores").toUriString();
+        var creditCheckResponse = restTemplate.postForObject(uri,
                 new CreditCheckRequest(citizenNumber),
                 CreditCheckResponse.class);
         if (creditCheckResponse.getScore() == HIGH && applyForCreditCardRequest.getCardType() == GOLD) {
             return new ApplyForCreditCardResponse(GRANTED);
         }
         throw new RuntimeException("Error occurred");
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity handleMethodArgumentNotValidException(
-            Exception error ) {
-        System.out.println(error.getMessage());
-        return new ResponseEntity<>(HttpStatus.BAD_GATEWAY);
     }
 }
--- credit/src/test/java/com/learning/credit/CreditApplicationTests.java
+++ credit/src/test/java/com/learning/credit/CreditApplicationTests.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties.StubsMode.LOCAL;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -15,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureStubRunner(ids = "com.learning:creditCheckService:+:stubs:8080", stubsMode = LOCAL)
 class CreditApplicationTests {
 
 	@Autowired
@@ -32,9 +36,10 @@ class CreditApplicationTests {
 						))
 				.andDo(print())
 				.andExpect(status().isOk())
+//				.andDo(print())
 				.andExpect(content().json(
 						"{" +
-								"\"+status\": \"GRANTED\"" +
+								"\"status\": \"GRANTED\"" +
 								"}"))
 				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
 	}
