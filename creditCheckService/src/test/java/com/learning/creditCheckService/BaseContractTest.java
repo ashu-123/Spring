4e0fff181f1048305c49619fa8e495629c49e593
Ashutosh <Ashutosh>
2024-09-15 15:42:26 +0530

    Addind more  contracts to support multiple contracts and scenarios

--- creditCheckService/src/test/java/com/learning/creditCheckService/BaseContractTest.java
+++ creditCheckService/src/test/java/com/learning/creditCheckService/BaseContractTest.java
@@ -7,6 +7,7 @@ import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeAll;
 
 import static com.learning.creditCheckService.model.CreditCheckResponse.Score.HIGH;
+import static com.learning.creditCheckService.model.CreditCheckResponse.Score.LOW;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -16,6 +17,7 @@ public class BaseContractTest {
     public static void setup() {
         final CreditCheckService creditCheckService = mock(CreditCheckService.class);
         when(creditCheckService.doCreditCheck(1234)).thenReturn(new CreditCheckResponse(HIGH));
+        when(creditCheckService.doCreditCheck(4444)).thenReturn(new CreditCheckResponse(LOW));
         RestAssuredMockMvc.standaloneSetup(new CreditCheckController(creditCheckService));
     }
 }
---
+++ creditCheckService/src/test/resources/contracts/credit/shouldGrantACreditScoreOfLowToACitizen.groovy
@@ -0,0 +1,30 @@
+package contracts.credit
+
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    request {
+        method 'POST'
+        url '/credit-scores'
+        body """
+        {
+            "citizenNumber" : 4444
+        }
+        """
+        headers {
+            contentType applicationJson()
+        }
+    }
+
+    response {
+        status 200
+        body """
+        {
+            "score" : "LOW"
+        }
+        """
+        headers {
+            contentType applicationJson()
+        }
+    }
+}
