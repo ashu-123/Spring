d85a4e5448cf588f29d19e6d9259b75cb030cdb6
Ashutosh <Ashutosh>
2025-05-17 11:47:59 +0530

    chat application using websockets in spring

---
+++ team-chat/src/main/java/com/learning/chat/TeamChatApplication.java
@@ -0,0 +1,13 @@
+package com.learning.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamChatApplication.class, args);
+	}
+
+}
