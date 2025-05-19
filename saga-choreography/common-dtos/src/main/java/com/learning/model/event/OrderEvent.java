4dbd419d122a4bfde953d36b8fa2ad5a8ff98a31
Ashutosh <Ashutosh>
2025-05-19 20:19:29 +0530

    Added model dtos to the commons-dto sub module

---
+++ saga-choreography/common-dtos/src/main/java/com/learning/model/event/OrderEvent.java
@@ -0,0 +1,31 @@
+package com.learning.model.event;
+
+import com.learning.model.dto.OrderRequestDto;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
+@NoArgsConstructor
+public class OrderEvent implements Event{
+
+    private UUID eventId = UUID.randomUUID();
+    private Date date = new Date();
+    private OrderRequestDto orderRequestDto;
+    private OrderStatus orderStatus;
+
+    public OrderEvent(OrderRequestDto orderRequestDto, OrderStatus orderStatus) {
+        this.orderRequestDto = orderRequestDto;
+        this.orderStatus = orderStatus;
+    }
+
+    @Override
+    public UUID getUUID() {
+        return eventId;
+    }
+
+    @Override
+    public Date getDate() {
+        return date;
+    }
+}
