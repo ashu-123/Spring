05b00ad7ab543564b169ff85c0559069960cbc6d
Ashutosh <Ashutosh>
2025-05-20 04:59:21 +0530

    Performed minor refactoring

--- saga-choreography/order-service/src/main/java/com/learning/order/config/OrderEventPublisherConfig.java
+++ saga-choreography/order-service/src/main/java/com/learning/order/config/OrderEventPublisherConfig.java
@@ -9,7 +9,7 @@ import reactor.core.publisher.Sinks;
 import java.util.function.Supplier;
 
 @Configuration
-public class OrderPublisherConfig {
+public class OrderEventPublisherConfig {
 
     @Bean
     public Sinks.Many<OrderEvent> orderSinks() {
