295a067f7f32363012e040236fa4af1014f3dac4
Ashutosh <Ashutosh>
2025-05-19 21:51:19 +0530

    Added rest end points to create order and get all orders

---
+++ saga-choreography/order-service/src/main/java/com/learning/order/mapper/PurchaseOrderMapper.java
@@ -0,0 +1,17 @@
+package com.learning.order.mapper;
+
+import com.learning.model.dto.OrderRequestDto;
+import com.learning.model.event.OrderStatus;
+import com.learning.order.entiry.PurchaseOrder;
+
+public class PurchaseOrderMapper {
+
+    public static PurchaseOrder orderDtoToEntity(OrderRequestDto orderRequestDto) {
+        PurchaseOrder purchaseOrder = new PurchaseOrder();
+        purchaseOrder.setPrice(orderRequestDto.getAmount());
+        purchaseOrder.setUserId(orderRequestDto.getUserId());
+        purchaseOrder.setProductId(orderRequestDto.getProductId());
+        purchaseOrder.setOrderStatus(OrderStatus.ORDER_CREATED);
+        return purchaseOrder;
+    }
+}
