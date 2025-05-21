9389596fca58229015c08b6bfdfd10b21ee071f5
Ashutosh <Ashutosh>
2025-05-21 06:21:09 +0530

    corrected configs and added resource layer

--- service-product-command/src/main/java/com/learning/command/model/entity/Product.java
+++ service-product-command/src/main/java/com/learning/command/model/entity/Product.java
@@ -1,22 +1,18 @@
 package com.learning.command.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-@Entity
-@Table(name = "PRODUCT_COMMAND")
+@Document("PRODUCT+COMMAND")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
 
     @Id
-    @GeneratedValue
     private Long id;
     private String name;
     private String description;
--- service-product-command/src/main/java/com/learning/command/repository/ProductRepository.java
+++ service-product-command/src/main/java/com/learning/command/repository/ProductRepository.java
@@ -1,9 +1,9 @@
 package com.learning.command.repository;
 
 import com.learning.command.model.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends MongoRepository<Product, Long> {
 }
