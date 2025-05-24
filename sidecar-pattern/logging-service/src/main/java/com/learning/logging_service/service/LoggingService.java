0691c4dbf094902422e2a2cadab771994b6f30bb
Ashutosh <Ashutosh>
2025-05-24 09:21:42 +0530

    Added lombok to the project to log different events throughtout the runtime of the application

--- sidecar-pattern/logging-service/src/main/java/com/learning/logging_service/service/LoggingService.java
+++ sidecar-pattern/logging-service/src/main/java/com/learning/logging_service/service/LoggingService.java
@@ -1,5 +1,6 @@
 package com.learning.logging_service.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -7,24 +8,30 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Date;
 
 @Service
+@Slf4j
 public class LoggingService {
 
     public static final String LOG_PATH = "/logs/book-service.log";
 
+    public LoggingService() {
+        log.info("Logging service booted up...");
+    }
+
     @Scheduled(fixedRate = 60000)
     public void processLogs() {
-        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(LOG_PATH))) {
+        log.info("Logging service scheduler triggered at {} ", new Date());
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(LOG_PATH))) {
             String line;
 
-            while((line = bufferedReader.readLine())!=null) {
+            while ((line = bufferedReader.readLine()) != null) {
                 //simulate pushing logs to a centralized service e.g, splunk/ELK
-                System.out.println("Processing log : " + line);
+                log.info("Processing log : " + line);
             }
-        }
-        catch (IOException ioException) {
-
+        } catch (IOException ioException) {
+            log.error(ioException.getMessage());
         }
     }
 }
