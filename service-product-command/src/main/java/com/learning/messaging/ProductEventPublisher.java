a8759e680274e26c0e7dd089ab9314689f40885f
Ashutosh <Ashutosh>
2025-05-25 13:09:43 +0530

    Allowing topic names to be configurable

--- service-product-command/src/main/java/com/learning/messaging/ProductEventPublisher.java
+++ service-product-command/src/main/java/com/learning/messaging/ProductEventPublisher.java
@@ -1,5 +1,6 @@
 package com.learning.messaging;
 
+import com.learning.config.TopicConfiguration;
 import com.learning.model.EventType;
 import com.learning.model.entity.Product;
 import com.learning.model.event.ProductEvent;
@@ -11,13 +12,17 @@ public class ProductEventPublisher {
 
     private final KafkaTemplate<String, ProductEvent> kafkaTemplate;
 
-    public ProductEventPublisher(KafkaTemplate<String, ProductEvent> kafkaTemplate) {
+    private final TopicConfiguration topicConfiguration;
+
+    public ProductEventPublisher(KafkaTemplate<String, ProductEvent> kafkaTemplate,
+                                 TopicConfiguration topicConfiguration) {
         this.kafkaTemplate = kafkaTemplate;
+        this.topicConfiguration = topicConfiguration;
     }
 
     public Product publishProductEvent(Product product, EventType eventType) {
         var productEvent = new ProductEvent(eventType.getEventType(), product);
-        kafkaTemplate.send("product-event-topic", productEvent);
+        kafkaTemplate.send(topicConfiguration.getName(), productEvent);
 
         return product;
     }
