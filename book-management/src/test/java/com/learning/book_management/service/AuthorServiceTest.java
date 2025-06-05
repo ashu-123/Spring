6084c8aa87b00a54cc96e3ec85219b364cf810ab
Ashutosh <Ashutosh>
2025-06-05 15:52:23 +0530

    Added test to verify the behavior of the relationship mapping

--- book-management/src/test/java/com/learning/book_management/service/AuthorServiceTest.java
+++ book-management/src/test/java/com/learning/book_management/service/AuthorServiceTest.java
@@ -1,5 +1,6 @@
 package com.learning.book_management.service;
 
+import com.learning.book_management.repository.BookRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +15,15 @@ class AuthorServiceTest {
     @Autowired
     private AuthorService authorService;
 
+    @Autowired
+    private BookRepository bookRepository;
+
     @Test
     void testCreateAuthor() {
         String authorName = "Ashutosh Mishra";
         List<String> bookTitles = List.of("Effective Java", "Java concurrency in Action");
         var author = authorService.createAuthorWithBooks(authorName, bookTitles);
         assertEquals(2, author.getBooks().size());
+        assertEquals(2l, bookRepository.count());
     }
 }
