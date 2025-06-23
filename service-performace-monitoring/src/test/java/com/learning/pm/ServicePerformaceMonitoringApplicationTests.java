7e25203f0c6a10c6b9eae06a2825716fac66b14f
Ashutosh <Ashutosh>
2025-06-23 10:12:17 +0530

    Initialized the project with basic strawman structure

---
+++ service-performace-monitoring/src/test/java/com/learning/pm/ServicePerformaceMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.pm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicePerformaceMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
