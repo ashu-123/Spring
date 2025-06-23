ff3d1b3e61a707f5d3bd52fd603fbb47985fb63c
Ashutosh <Ashutosh>
2025-06-23 10:36:36 +0530

    Made max string length and max collection size capacity configurable parameters

--- service-performace-monitoring/src/main/java/com/learning/pm/config/PerformanceProperties.java
+++ service-performace-monitoring/src/main/java/com/learning/pm/config/PerformanceProperties.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "performance")
 public class PerformanceProperties {
     private long thresholdMs;
+    private int maxStringLength = 100;
+    private int maxCollectionSize = 10;
+    private int maxMapSize = 10;
+
 
     public long getThresholdMs() {
         return thresholdMs;
@@ -14,6 +18,33 @@ public class PerformanceProperties {
 
     public void setThresholdMs(long thresholdMs) {
         this.thresholdMs = thresholdMs;
+    }
+
+    public int getMaxStringLength() {
+        return maxStringLength;
+    }
+
+    public PerformanceProperties setMaxStringLength(int maxStringLength) {
+        this.maxStringLength = maxStringLength;
+        return this;
+    }
+
+    public int getMaxCollectionSize() {
+        return maxCollectionSize;
+    }
+
+    public PerformanceProperties setMaxCollectionSize(int maxCollectionSize) {
+        this.maxCollectionSize = maxCollectionSize;
+        return this;
+    }
+
+    public int getMaxMapSize() {
+        return maxMapSize;
+    }
+
+    public PerformanceProperties setMaxMapSize(int maxMapSize) {
+        this.maxMapSize = maxMapSize;
+        return this;
     }
 }
 
